e3f46bdb3bf0dd65a2958434801412a0f155a82b
Nick Pantic <nmpantic@cpp.edu>
2016-02-22 02:25:44 -0800

    first commit

---
+++ Constants.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const double PI = 3.14159265358979323846264338;
+const double G = 6.674e-11;
+
+const int DEFAULT_WIDTH = 800;
+const int DEFAULT_HEIGHT = 600;
+
+const int PARTICLE_COUNT = 50;
+const double MAX_MASS = 1.0e14;
+
+#endif
---
+++ Game.cpp
@@ -0,0 +1,169 @@
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#include "Game.h"
+
+Game::Game() 
+    : width(DEFAULT_WIDTH), height(DEFAULT_HEIGHT), 
+      start(0), last(0), current(0), 
+      good(true), running(false), 
+      particles(std::vector<Particle>())
+{
+    // Seed the random number generator
+    srand(time(0));
+    
+    // initialize SDL
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        good = false;
+        return;
+    }
+    
+    // initialize SDL window
+    window = SDL_CreateWindow("Gravity", SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
+    if (window == NULL)
+    {
+        good = false;
+        return;
+    }
+    
+    // initialize SDL renderer
+    renderer = SDL_CreateRenderer(window, -1, 
+            SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL)
+    {
+        good = false;
+        return;
+    }
+    
+    // initialize particle texture
+    SDL_Surface* bmp = SDL_LoadBMP("particle.bmp");
+    if (bmp == NULL)
+    {
+        good = false;
+        return;
+    }
+    particleTexture = SDL_CreateTextureFromSurface(renderer, bmp);
+    SDL_FreeSurface(bmp);
+    if (particleTexture == NULL)
+    {
+        good = false;
+        return;
+    }
+    
+    // initialize our particles
+    for (int i = 0; i < PARTICLE_COUNT; ++i)
+    {
+        particles.push_back(randomParticle());
+    }
+}
+
+Game::~Game()
+{
+    if (!good)
+    {
+        std::cout << "SDL Error: " << SDL_GetError() << std::endl;
+    }
+    if (particleTexture != NULL)
+    {
+        SDL_DestroyTexture(particleTexture);
+    }
+    if (renderer != NULL)
+    {
+        SDL_DestroyRenderer(renderer);
+    }
+    if (window != NULL)
+    {
+        SDL_DestroyWindow(window);
+    }
+    SDL_Quit();
+}
+
+int Game::operator()()
+{
+    if (!good)
+    {
+        return -1;
+    }
+    running = true;
+    SDL_Event event;
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    start = SDL_GetTicks();
+    last = start;
+
+    while (running) // every iteration is 1 frame
+    {
+        current = SDL_GetTicks();
+        while (SDL_PollEvent(&event))
+        {
+            handleEvent(event);
+        }
+        update((current - last) / 1000.0);
+        render();
+        last = current;
+    }
+
+    return 0;
+}
+
+// dt is time since last frame in seconds
+void Game::update(double dt)
+{
+    std::cout << dt << " sec since last frame.\n";
+
+    // Replace with your game logic!
+}
+
+void Game::render()
+{
+    SDL_RenderClear(renderer);
+    
+    // rendering here would place objects beneath the particles
+    
+    for (const Particle& p : particles)
+    {
+        drawParticle(p);
+    }
+    
+    // rendering here would place objects on top of the particles
+    
+    SDL_RenderPresent(renderer);
+}
+
+void Game::handleEvent(const SDL_Event& event)
+{
+    switch (event.type)
+    {
+    // Add your own event handling here if desired
+    case SDL_QUIT:
+        running = false;
+        break;
+    default:
+        break;
+    }
+}
+
+void Game::drawParticle(const Particle& p)
+{
+    SDL_Rect dst;
+    double shift = p.getRadius();
+    dst.x = (int) (p.getPos().getX() - shift);
+    dst.y = (int) (p.getPos().getY() - shift);
+    dst.w = shift * 2;
+    dst.h = shift * 2;
+    SDL_RenderCopy(renderer, particleTexture, NULL, &dst);
+}
+
+Particle Game::randomParticle() const
+{
+    Point pos(rand() % width, rand() % height);
+    
+    // using random percentage of MAX_MASS
+    double mass = ((double) rand() / RAND_MAX) * MAX_MASS + 1.0;
+    
+    return Particle(pos, mass);
+}
